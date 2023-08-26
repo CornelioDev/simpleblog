c9ec4571f4c3b9541b116482950da912280abab9
José Manuel Cornelio <cornelio@proton.me>
2023-08-26 16:16:23 -0400

    Simplefied blog posts feed

--- components/Blog/BlogPostFeed.jsx
+++ components/Blog/BlogPostFeed.jsx
@@ -1,13 +1,13 @@
 'use client'
 
+import Link from "next/link"
+
 const BlogPostFeed = ({ post }) => {
     return (
         <div className="blog-post-feed">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-            <button onClick={
-                () => { alert('Hello World!') }
-            }>read more...</button>
+            <Link href={`/blog/${post.id}`}>
+                <h1>{post.id}. {post.title}</h1>
+            </Link>
         </div>
     )
 }
