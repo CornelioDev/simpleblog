513ffc4a416a81938f2e66b50f3cd0ee0e58255c
José Manuel Cornelio <cornelio@proton.me>
2023-08-27 11:25:08 -0400

    Blog feed

--- components/Blog/BlogFeed.jsx
+++ components/Blog/BlogFeed.jsx
@@ -1,17 +1,19 @@
 import BlogPostFeed from "./BlogPostFeed"
 
 const loadPosts = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const postsJson = await res.json()
-    return postsJson
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const postsJson = await res.json()
+  return postsJson
 }
 
 const BlogFeed = async () => {
-    const blogPosts = await loadPosts()
+  const blogPosts = await loadPosts()
   return (
-    blogPosts.map( (blogPost) => (
-      <BlogPostFeed post={blogPost} key={blogPost.id}/>
-    ))
+    <div className="feed flex flex-col gap-2">
+      {blogPosts.map((blogPost) => (
+        <BlogPostFeed post={blogPost} key={blogPost.id} />
+      ))}
+    </div>
   )
 }
 
--- components/Blog/BlogPostFeed.jsx
+++ components/Blog/BlogPostFeed.jsx
@@ -4,10 +4,14 @@ import Link from "next/link"
 
 const BlogPostFeed = ({ post }) => {
     return (
-        <div className="blog-post-feed">
+        <div className="blog-post bg-gray-600 p-3 text-gray-100 rounded-sm text-xl font-semibold">
             <Link href={`/blog/${post.id}`}>
-                <h1>{post.id}. {post.title}</h1>
+                <h1>{post.title}</h1>
             </Link>
+            <div className="flex place-content-between text-xs mt-4 text-gray-400">
+                <div className="likes">100 Likes</div>
+                <div className="comments">10 Comments</div>
+            </div>
         </div>
     )
 }
